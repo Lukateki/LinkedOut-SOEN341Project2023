b7593ce38abc5e32bb028b312607ca489c5e0c7f
LiamDaigle <liam.daigle@gmail.com>
2023-02-17 16:02:27 -0500

    TASK-00-02 : Created Candidate Database

--- Backend/credentials/models.py
+++ Backend/credentials/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
