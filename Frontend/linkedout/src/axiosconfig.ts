cef77e0ff14fb5942eaed57033e4ba31768c429f
DanielC2001 <daniel-carcamo1@hotmail.com>
2023-02-17 13:56:27 -0500

    Frontend-Setup Updated axios api url

--- Frontend/linkedout/src/axiosconfig.ts
+++ Frontend/linkedout/src/axiosconfig.ts
@@ -1,10 +1,10 @@
 const axios = require(`axios`);
-
+const api = "http://127.0.0.1:8000/api/v1";
 //Example of how to do request
 
 const dummyGETRequest = async () => {
     //The params property has to be an object with the params needed for the request
-    axios.get('/api-route', { params: {}})
+    axios.get(api + '/api-route', { params: {}})
         .then(result => {
             console.log(result);
         }).catch(error => {
@@ -17,7 +17,7 @@ const dummyGETRequest = async () => {
 
 const dummyPOSTRequest = async () => {
     const obj = {};//This object has all the necessary parameters, you include the properties
-    axios.post('/api-route', obj)
+    axios.post(api + '/api-route', obj)
     .then(result => {
         console.log(result);
     }).catch(error => {
