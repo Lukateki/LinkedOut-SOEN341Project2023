ce26774abcb0c87c0866b37cdd6a788042c060df
sfhelmet <sfhelmet@gmail.com>
2023-02-17 17:02:49 -0500

    Update pipfile and Job model for flexibility

--- Backend/JobListings/models.py
+++ Backend/JobListings/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Job(models.Model):
     title = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+    url = models.TextField(max_length=256)
     posting_date = models.DateField()
     city = models.CharField(max_length=100) # table for cities with province and country
     job_type = models.CharField(max_length=100)
-    description = models.CharField(max_length=2560)
+    description = models.TextField(max_length=256)
 
     def __str__(self):
         return self.title
