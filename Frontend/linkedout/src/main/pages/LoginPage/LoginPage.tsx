c4b844843fa90ffcfd9e61e203bdbd05802fdfa3
Fadi Nimer <fadi_nimer@hotmail.com>
2023-02-27 08:38:49 -0500

    Added Student/Employer Toggle

--- Frontend/linkedout/src/main/pages/LoginPage/LoginPage.tsx
+++ Frontend/linkedout/src/main/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, TextField, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { useLogin } from './hooks';
 import './LoginPage.css';
 
 const LoginPage = () => {
     const { handleLoginBtnClick } = useLogin();
+    const [userType, setUserType] = useState('student'); // set initial value to student
+    const handleUserTypeChange = (event, newUserType) => {
+      setUserType(newUserType);
+    };
     return (
         <div className='login-page-wrapper'>
             <h2 className='linkedout-banner'>LinkedOut</h2>
@@ -18,6 +22,20 @@ const LoginPage = () => {
                     label="Password"
                     type="password"
                 />
+                <ToggleButtonGroup
+                  value={userType}
+                  exclusive
+                  onChange={handleUserTypeChange}
+                  aria-label="user type"
+                  className='user-type-field'
+                >
+                  <ToggleButton value="student" aria-label="student">
+                    Student
+                  </ToggleButton>
+                  <ToggleButton value="employer" aria-label="employer">
+                    Employer
+                  </ToggleButton>
+                </ToggleButtonGroup>
                 <Button 
                     className='login-btn'
                     variant='contained'
