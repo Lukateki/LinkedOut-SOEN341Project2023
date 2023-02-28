3e01ac9db0d2b72be271c4617e35860eee474f29
Fadi Nimer <fadi_nimer@hotmail.com>
2023-02-28 15:23:36 -0500

    Added jobpostings list elements

--- Backend/LinkedOut/JobListings/views.py
+++ Backend/LinkedOut/JobListings/views.py
@@ -1,9 +1,6 @@
-from rest_framework import viewsets, permissions
-from .models import Job
-from .serializers import JobSerializer
 
 # Create your views here.
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
--- Frontend/linkedout/src/main/pages/HomePage/HomePage.tsx
+++ Frontend/linkedout/src/main/pages/HomePage/HomePage.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { get_all_jobs } from '../../../axiosconfig';
 import { useNavigate } from 'react-router-dom';
-
 import './HomePage.css';
 
 const HomePage = () => {
 
   const navigate = useNavigate();
+  const [jobPostings, setJobPostings] = useState([]);
 
   const handleLoginClick = () => {
     navigate('/login');
   };
-
   const handleRegisterClick = () => {
     navigate('/register');
   };
+
+  useEffect(() => {
+    const fetchJobPostings = async () => {
+      try {
+        const response = await get_all_jobs();
+        setJobPostings(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchJobPostings();
+  }, []);
 
   return (
     <div className="home-container">
@@ -29,7 +42,14 @@ const HomePage = () => {
       <div className="home-content">
         <h1>Welcome to LinkedOut</h1>
         <h2>Available Job Postings</h2>
-        <ul /* job postings contents to be added here */>
+        <ul className="homepage-job-postings">
+          {jobPostings.map((jobPosting) => (
+            <li key={jobPosting.id}>
+              <h3>{jobPosting.title}</h3>
+              <p>{jobPosting.description}</p>
+              {/* Include an apply button (blocked for users that are not authenticated) */}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
