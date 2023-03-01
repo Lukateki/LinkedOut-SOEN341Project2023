3e846cd7abf2e38c1cd3b6dc3aeb232ca7d5f771
clara-gagnon <clarag2002@gmail.com>
2023-02-28 22:22:22 -0500

    register an applicant or employer
    - does not process if a field is left empty or if email is invalid
    - adds new user to the database in appropriate section
    - seperated full name into first and last name

--- Frontend/linkedout/src/axiosconfig.ts
+++ Frontend/linkedout/src/axiosconfig.ts
@@ -40,10 +40,12 @@ export const candidate_login = async (username: string, password: string) => {
 }
 
 export const get_all_jobs = async () => {
-    return axios.get(api + '/jobs');
-}
+    const authToken = new Cookies().get("auth-token");
+    const headers = { 'Authorization': `Token ${authToken}` }
+    return axios.get(api + '/candidates', { headers });
 }
 
+//for the applicant/student/candidate registration
 export const register_applicant = async () => {
     return axios.post(api + "/applicants/", {skills: undefined, interests: undefined, resume: undefined})
 }
