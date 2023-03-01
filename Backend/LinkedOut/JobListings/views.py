30a2a23e3b79c323718f8536d331aa3d0e03e686
sfhelmet <sfhelmet@gmail.com>
2023-03-01 01:43:34 -0500

    initializers to each app

---
+++ Backend/LinkedOut/JobListings/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets, permissions
+from .models import Job
+from .serializers import JobSerializer
+
+# Create your views here.
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
