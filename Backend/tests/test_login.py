89f5385b7fdc0cc39e00e75977d0b0a702104bc5
sfhelmet <sfhelmet@gmail.com>
2023-03-01 02:15:34 -0500

    pytest trying

---
+++ Backend/tests/test_login.py
@@ -0,0 +1,16 @@
+import pytest
+
+from django.contrib.auth.models import User
+# function Run once per test
+# class    Run once per class of tests
+# module   Run once per module
+# session  Run once per session
+
+@pytest.fixture()
+def user_1(db):
+    return User.objects.create_user("test-user")
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create_user(username='test', email='test@test.com', password='test')
+    assert User.objects.count() == 1
