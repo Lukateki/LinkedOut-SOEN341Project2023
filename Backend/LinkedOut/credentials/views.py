eab2e3bd650fc0021dff1471b0f1a88682111220
LiamDaigle <liam.daigle@gmail.com>
2023-03-16 22:56:21 -0400

    TASK-05-01: Functioning Email Sender

--- Backend/LinkedOut/credentials/views.py
+++ Backend/LinkedOut/credentials/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 from LinkedOut.credentials.models import Applicant, Education, Experience, Recruiter
 from .serializers import ApplicantSerializer, EducationSerializer, ExperienceSerializer, RecruiterSerializer, UserSerializer, GroupSerializer
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+import re
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -58,4 +65,43 @@ class ExperienceViewSet(viewsets.ModelViewSet):
     serializer_class = ExperienceSerializer
     # permission_classes = [permissions.IsAuthenticated]
 
+class SendEmailView(APIView):
+
+    def get(self, request: Request):
+        email_regex = re.compile(r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+")
+        query_params = request.query_params
+
+        if not query_params['firstname']:
+            return Response("Missing first name!")
+        first_name = query_params['firstname']
+        
+        if not query_params['lastname']:
+            return Response("Missing last name!")
+        last_name = query_params['lastname']
+        
+        if not query_params['company']:
+            return Response({"status": "Missing company name!"})
+        company = query_params['company']
+
+        recipient = query_params['email']
+        if re.fullmatch(email_regex, recipient):
+            sender = 'linkedoutnotifications'
+            password = 'lkcpvpfjtfwvnuwp'
+
+            email = EmailMessage()
+            email['From'] = sender
+            email['To'] = recipient
+            email['Subject'] = "You got an Interview!"
+
+            email.set_content(f"Hello {first_name} {last_name}\nYou got an interview from {company}! Head over to LinkedOut to respond!")
+
+            context = ssl.create_default_context()
+
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context= context) as smtp:
+                smtp.login(sender, password)
+                smtp.sendmail(sender, recipient, email.as_string())
+            return Response("Successfully sent email!")
+        
+        else:
+            return Response("Recipient's email address does not have a valid email structure, or was not provided")
  
