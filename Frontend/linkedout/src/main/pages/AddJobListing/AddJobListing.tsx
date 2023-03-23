8b71ac42a8e43819933c7984e78b52dcf914370b
clara-gagnon <clarag2002@gmail.com>
2023-03-23 17:45:14 -0400

    Made form as a card

--- Frontend/linkedout/src/main/pages/AddJobListing/AddJobListing.tsx
+++ Frontend/linkedout/src/main/pages/AddJobListing/AddJobListing.tsx
@@ -4,6 +4,7 @@ import NavBar from '../../../components/NavBar/NavBar';
 import { Navigate } from "react-router-dom";
 import { Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
+import { Card } from '@mui/material';
 import './AddJobListing.css';
 import { useUploadJob } from './hooks';
 
@@ -33,84 +34,86 @@ const AddJobListingPage = () => {
         <div className="add-job-container">
             <NavBar/>
             <div className="add-job-content">
-                <h2>Create a New Job Listing</h2>
-                <div className='double-columns'>
-                    <TextField
-                        className='job-title'
-                        label="Job Title"
-                        value={title}
-                        onChange={handleTitleChange}
-                    />
-                    <TextField
-                        className='job-recruiter'
-                        label="Recruiting Company"
-                        value={recruiter}
-                        onChange={handleRecruiterChange}
-                    />
-                </div>
-                <div className='double-columns'>
-                    <TextField
-                        className='job-type'
-                        select
-                        SelectProps={{    
-                            native:true
-                        }}
-                        value={type}
-                        onChange={handleTypeChange}
+                <Card className='card'>
+                    <h2>Create a New Job Listing</h2>
+                    <div className='double-columns'>
+                        <TextField
+                            className='job-title'
+                            label="Job Title"
+                            value={title}
+                            onChange={handleTitleChange}
+                        />
+                        <TextField
+                            className='job-recruiter'
+                            label="Recruiting Company"
+                            value={recruiter}
+                            onChange={handleRecruiterChange}
+                        />
+                    </div>
+                    <div className='double-columns'>
+                        <TextField
+                            className='job-type'
+                            select
+                            SelectProps={{    
+                                native:true
+                            }}
+                            value={type}
+                            onChange={handleTypeChange}
 
+                        >
+                            {jobTypes.map((option) => (
+                                <option key={option.value} value={option.value}>
+                                    {option.label}
+                                </option>
+                            ))}
+                        </TextField>
+                        <TextField
+                            className='job-location'
+                            label="location(city)"
+                            value={city}
+                            onChange={handleCityChange}
+                        />
+                    </div>
+                    <div className='double-columns'>
+                        <TextField
+                            className='job-posting-date'
+                            label="Current Date"
+                            value={date}
+                            onChange={handleDateChange}
+
+                        />
+                        <TextField
+                            className='job-expiry-date'
+                            label="Expiry Date"
+                            value={expiry}
+                            onChange={handleExpiryChange}
+
+                        />
+                    </div>
+                    <TextField
+                        className='job-description'
+                        label="Description"
+                        multiline
+                        rows={4}
+                        value={description}
+                        onChange={handleDescriptionChange}
+
+                    />
+                    <TextField
+                        className='job-url'
+                        label="Url"
+                        value={url}
+                        onChange={handleUrlChange}
+
+                    />
+                    <Button 
+                        className='upload-job-btn'
+                        variant='contained'
+                        onClick={handleUploadJobBtnClick}
                     >
-                        {jobTypes.map((option) => (
-                            <option key={option.value} value={option.value}>
-                                {option.label}
-                            </option>
-                        ))}
-                    </TextField>
-                    <TextField
-                        className='job-location'
-                        label="location(city)"
-                        value={city}
-                        onChange={handleCityChange}
-                    />
-                </div>
-                <div className='double-columns'>
-                    <TextField
-                        className='job-posting-date'
-                        label="Current Date"
-                        value={date}
-                        onChange={handleDateChange}
-
-                    />
-                    <TextField
-                        className='job-expiry-date'
-                        label="Expiry Date"
-                        value={expiry}
-                        onChange={handleExpiryChange}
-
-                    />
-                </div>
-                <TextField
-                    className='job-description'
-                    label="Description"
-                    multiline
-                    rows={4}
-                    value={description}
-                    onChange={handleDescriptionChange}
-
-                />
-                <TextField
-                    className='job-url'
-                    label="Url"
-                    value={url}
-                    onChange={handleUrlChange}
-
-                />
-                <Button 
-                    className='upload-job-btn'
-                    variant='contained'
-                    onClick={handleUploadJobBtnClick}
-                >
-                    Upload Job
-                </Button> 
+                        Upload Job
+                    </Button> 
+                </Card>
             </div>
         </div>
 
